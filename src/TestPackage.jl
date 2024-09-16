300314d7b1afe315a50f2c01d07daae30014b066
Paul Schrimpf <schrimpf@mail.ubc.ca>
2024-09-16 10:41:06 -0700

    added integration code and some initial tests

--- src/TestPackage.jl
+++ src/TestPackage.jl
@@ -1,11 +1,54 @@
 module TestPackage
 
-"""
-    hi = hello_world()
-A simple function to return "Hello, World!"
-"""
-function hello_world()
-    return "Hello, World!"
+using Distributions, Statistics
+using HCubature
+import Sobol
+import Base.Iterators: take
+using FastGaussQuadrature, LinearAlgebra
+import Base.Iterators: product, repeated
+using SparseGrids
+
+export ∫mc, ∫s, ∫q, ∫sgq, ∫cuba
+
+
+∫mc(f, dx; ndraw=100)=mean(f(rand(dx)) for i in 1:ndraw)
+
+function ∫s(f,dx::AbstractMvNormal;ndraw=100)
+  marginals = [Normal(dx.μ[i], sqrt(dx.Σ[i,i])) for i in 1:length(dx)]
+  invcdf(x) = quantile.(marginals,x)
+  ss = skip(Sobol.SobolSeq(length(dx)),ndraw)
+  mean(f(invcdf(x)) for x in take(ss,ndraw))
 end
+
+function ∫s(f,dx::Normal;ndraw=100)
+  invcdf(x) = quantile(dx,x)
+  ss = skip(Sobol.SobolSeq(length(dx)),ndraw)
+  mean(f(invcdf(x[1])) for x in take(ss,ndraw))
+end
+
+function ∫q(f, dx::MvNormal; ndraw=100)
+  n = Int(ceil(ndraw^(1/length(dx))))
+  x, w = gausshermite(n)
+  L = cholesky(dx.Σ).L
+  sum(f(√2*L*vcat(xs...) + dx.μ)*prod(ws)
+      for (xs,ws) ∈ zip(product(repeated(x, length(dx))...),
+                        product(repeated(w, length(dx))...))
+        )/(π^(length(dx)/2))
+end
+
+function ∫sgq(f, dx::MvNormal; order=5)
+  X, W = sparsegrid(length(dx), order, gausshermite, sym=true)
+  L = cholesky(dx.Σ).L
+  sum(f(√2*L*x + dx.μ)*w for (x,w) ∈ zip(X,W))/(π^(length(dx)/2))
+end
+
+
+function ∫cuba(f, dx; rtol=1e-4)
+  D = length(dx)
+  x(t) = t./(1 .- t.^2)
+  Dx(t) = prod((1 .+ t.^2)./(1 .- t.^2).^2)
+  hcubature(t->f(x(t))*pdf(dx,x(t))*Dx(t), -ones(D),ones(D), rtol=rtol)[1]
+end
+
 
 end
--- test/test-basic-test.jl
+++ test/test-basic-test.jl
@@ -1,3 +1,12 @@
 @testset "TestPackage.jl" begin
-    @test TestPackage.hello_world() == "Hello, World!"
+  using Distributions, LinearAlgebra
+  dx = Normal(0,1)
+  Σ = ones(2,2) + I
+  dX = MvNormal(zeros(2), Σ)
+  N = 10000
+  @test isapprox(∫mc(x->x^2, dx, ndraw=N), 1, rtol=6/sqrt(N))
+  @test isapprox(∫s(x->x^2, dx, ndraw=N), 1, rtol=6*log(N)/N)
+  @test isapprox(norm(∫mc(x->x*x', dX, ndraw=N) - Σ), 0, atol=12/sqrt(N))
+  @test isapprox(norm(∫s(x->x*x', dX, ndraw=N) - Σ), 0, atol=6*log(N)^2/N)
+
 end
